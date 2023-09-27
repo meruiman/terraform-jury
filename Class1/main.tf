81e52ac55d620a7b75026a5a1db04dff69c3a27f
meruiman <imankulova.meru@gmail.com>
2023-09-27 01:24:11 +0000

    new files

---
+++ Class1/main.tf
@@ -0,0 +1,20 @@
+#resource "aws_iam_user" "lb" {
+  name = "hello"
+}
+resource "aws_iam_user" "lb1" {
+  name = "hello1"
+}
+
+resource "aws_iam_group" "developers" {
+ name = "developers"
+}
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb.name,
+    aws_iam_user.lb1.name,
+  ]
+
+  group = aws_iam_group.developers.name
+}
